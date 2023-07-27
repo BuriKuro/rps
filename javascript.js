a6661e44c53b7ae351c82b204ef51e70df4ca285
Brittani <kisaraghi@gmail.com>
2023-07-27 11:03:59 -0400

    Calculate winner after 5 rounds.

--- javascript.js
+++ javascript.js
@@ -2,43 +2,33 @@ console.log("Welcome, trainers!  Choose your Pokemon.")
 
 const starters = ["Bulbasaur", "Charmander", "Squirtle"];
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const select = starters[Math.floor(Math.random() * starters.length)];
     return select;
 }
 
-
-
-let computerChoice = getComputerChoice();
-
-let starterChoice = prompt("Who's your starter?");
-
-/*
-ChatGPT code
-function round() {
-    if (starterChoice.toLowerCase() === "bulbasaur" && computerChoice === "Squirtle") {
-      console.log("It's super effective!");
-    } else if (starterChoice.toLowerCase() === "charmander" && computerChoice === "Bulbasaur") {
-      console.log("It's super effective!");
-    } else if (starterChoice.toLowerCase() === "squirtle" && computerChoice === "Charmander") {
-      console.log("It's super effective!");
-    } else {
-      console.log("You uh, don't know what you're doing, huh?");
-    }
-  }
-  */
-
 function round() {
 
-    console.log(`Your opponent chose ${computerChoice}.`);
+    let computerChoice = getComputerChoice();
+    let starterChoice = prompt("Who's your starter?");
+
     console.log(`You chose ${starterChoice}.`);
+    console.log(`Your opponent chose ${computerChoice}.`);
+
+    let win = "It's super effective!"
+    let lose = "It's not every effective..."
+    let draw = "It's an even match!"
+
 
     if  (
         (starterChoice.toLowerCase() == "bulbasaur" && computerChoice == "Squirtle") ||
         (starterChoice.toLowerCase() == "charmander" && computerChoice == "Bulbasaur") ||
         (starterChoice.toLowerCase() == "squirtle" && computerChoice == "Charmander")
     )
-        {console.log("It's super effective!");
+        {console.log(win + " Your score is " + (++playerScore) + ". The computer score is " + computerScore + ".");
     }
 
     else if  (
@@ -46,13 +36,13 @@ function round() {
         (starterChoice.toLowerCase() == "squirtle" && computerChoice == "Bulbasaur") ||
         (starterChoice.toLowerCase() == "bulbasaur" && computerChoice == "Charmander")
     )
-        {console.log("It's not very effective...");
+        {console.log(lose + " Your score is " + playerScore + ". The computer score is " + (++computerScore) + ".");
     }
 
     else if  (
         (starterChoice.toLowerCase() == computerChoice.toLowerCase()) 
     )
-        {console.log("It's an even match!");
+        {console.log(draw + " Your score is " + playerScore + ". The computer score is " + computerScore + ".");
     }
 
     else if (
@@ -60,57 +50,29 @@ function round() {
         {console.log("We aren't counting Yellow.")}
 
 
-    else {console.log("That's not an original 151 starter, okay?")
+    else {console.log("You gotta pick a Red/Blue starter, man.")
     }
-
 }
 
 round();
+round();
+round();
+round();
+round();
 
+function result() {
+    if (
+        playerScore > computerScore && computerScore < playerScore)
+        {console.log("You defeated your opponent!");}
 
-/*Randomly return rock paper scissors 
+    else if (
+        playerScore === computerScore)
+        {console.log("It's a tie!");}
 
-let playerSelection = prompt("Who's your starter?");
-    Get player input via text box. 
+    else if (
+        computerScore > playerScore && playerScore < computerScore)
+        {console.log("You white out!")}
+    }
 
-playerSelection
-
-    if (playerSelection.toLowerCase() === "rock") and computerSelection = scissors OR
-    || (playerSelection.toLowerCase() === "paper") and computerSelection = rock OR
-    || (playerSelection.toLowerCase() === "scissors") and computerSelection = paper OR
+result();
     
-    alert("You win!")
-
-else if
-
-    if (playerSelection.toLowerCase() === "rock") and computerSelection = paper OR
-    || (playerSelection.toLowerCase() === "paper") and computerSelection = scissors OR
-    || (playerSelection.toLowerCase() === "scissors") and computerSelection = rock OR
-    
-    alert("You lose!")
-
-    playRound -> call function 5 times instead of looping
-        if player wins 3 or more times, player wins
-        if player wins 2 or less times, computer wins
-
-else
-    
-    alert("It's a tie!")
-    /*
-    
-    
-/* 
-ODIN CODE SUGGESTION
-//function playRound(playerSelection, computerSelection) {
-// your code here!
-}
- 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection)); 
-*/
-
-/*
-str1.toLowerCase() === str2.toLowerCase();
-    compare stings, case insensitive
-*/
